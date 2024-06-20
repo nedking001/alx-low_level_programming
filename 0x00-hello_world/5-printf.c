6b36083d1246bbe098510c6c464ec084f95b0725
Chinedum <chinedume77@gmail.com>
2024-06-20 16:09:13 -0700

    print sentence

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: returns 0 if successfull
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
