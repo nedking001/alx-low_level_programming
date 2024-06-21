f15d2228c785b6229ed01aaf23975c3acabcf5aa
Chinedum <chinedume77@gmail.com>
2024-06-20 17:01:53 -0700

    print byte size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)", sizeof(char))/n;
-	printf("Size of an int: %lu byte(s)", sizeof(int))/n;
-	printf("Size of a long int: %lu byte(s)", sizeof(long int))/n;
-	printf("Size of a long long int: %lu byte(s)", sizeof(long long int))/n;
-	printf("Size of a float: %lu byte(s)", sizeof(float))/n;
+	printf("Size of a char: %lu byte(s)", sizeof(char))\n;
+	printf("Size of an int: %lu byte(s)", sizeof(int))\n;
+	printf("Size of a long int: %lu byte(s)", sizeof(long int))\n;
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int))\n;
+	printf("Size of a float: %lu byte(s)", sizeof(float))\n;
 	return (0);
 
 }
