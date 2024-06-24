6b58c341e7fe1a97ece1cc981d58e6899a5fd582
Chinedum <chinedume77@gmail.com>
2024-06-24 03:02:00 -0700

    print 3 digit combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - contains main function
+ *
+ * Return: returns 0 upon success
+ */
+
+int main(void)
+{
+	int hundreds, tens, ones;
+
+	for (hundreds = 0; hundreds <= 7; hundreds++)
+	{
+		for (tens = hundreds + 1; tens <= 8; tens++)
+		{
+			for (ones = tens + 1; ones <= 9; ones++)
+			{
+				putchar(hundreds + '0');
+				putchar(tens + '0');
+				putchar(ones + '0');
+				if (hundreds != 7 || tens != 8 || ones != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
