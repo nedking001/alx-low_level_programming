293efc6982d97fc3d81dbfcd2538ec1cbd0a5735
Chinedum <chinedume77@gmail.com>
2024-06-24 03:10:35 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,15 @@
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf('\n');
 	printf("Size of an int: %lu byte(s)", sizeof(int));
+	printf('\n');
 	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf('\n');
 	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf('\n');
 	printf("Size of a float: %lu byte(s)", sizeof(float));
+	printf('\n');
 	return (0);
 
 }
