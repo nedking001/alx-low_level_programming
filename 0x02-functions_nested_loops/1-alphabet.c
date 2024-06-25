0419d1551afe736c2a0760b1bc56115d24f91ffe
Chinedum <chinedume77@gmail.com>
2024-06-25 07:49:56 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - contains the function to print alphabets
+ *
+ * Return: void return
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
