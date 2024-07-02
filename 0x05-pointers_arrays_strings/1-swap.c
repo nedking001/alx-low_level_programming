c7008e043ee6448a8a7f30a656e32bd09b89d7d8
Chinedum <chinedume77@gmail.com>
2024-07-01 21:37:32 -0700

    pointers task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps 2 integers
+ * @a: value 1
+ * @b: value 2
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+
+
+
+}
