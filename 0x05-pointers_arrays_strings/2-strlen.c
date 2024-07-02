e777db9391ef957d014eb71d1afe40cd1443bdc7
Chinedum <chinedume77@gmail.com>
2024-07-01 21:59:02 -0700

    pointers task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: value parsed
+ *
+ * Return: length of the string 
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0' ; i++)
+		count++;
+
+}
