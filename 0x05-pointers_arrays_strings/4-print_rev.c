bfeb78e0405c0c5f003d6461e7b77bb335a6d509
Chinedum <chinedume77@gmail.com>
2024-07-02 18:05:48 -0700

    pointers task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: Parameter parsed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (i = count - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+
+}
