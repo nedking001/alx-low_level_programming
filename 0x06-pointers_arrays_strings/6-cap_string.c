7ce7e477528fdcbaac281936aaabda83d84e9d91
Chinedum <chinedume77@gmail.com>
2024-07-04 18:39:30 -0700

    pointerstask7

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of string
+ * @str: string to be capitalized
+ * Return: 0
+ */
+
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ''|| str[index - 1] == '\t' || str[index - 1] == '\n' || str[index - 1] == ',' || str[index - 1] == ';' || str[index - 1] == '.' || str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' || str[index - 1] == '('|| str [index - 1] == ')' || str[index - 1] == '{'|| str[index - 1] == '}' || index == 0)
+			str[index] -= 32;
+		index++;
+	}
+	return (str);
+
+
+
+}
