0ca586242578447007937c4e6ccb2bab2656fc36
Chinedum <chinedume77@gmail.com>
2024-07-05 19:30:12 -0700

    task11

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,4 +13,6 @@ char *leet(char *);
 int _putchar(char c);
 void print_number(int n);
 char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 #endif
