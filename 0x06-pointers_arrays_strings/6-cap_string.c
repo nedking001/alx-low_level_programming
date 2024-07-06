31852e91423315110a820985e61ec575243acb6e
Chinedum <chinedume77@gmail.com>
2024-07-05 19:36:43 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * cap_string - function to capitalize string
- * @str: string to be capitalized
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
  *
- * Return: 0
+ * Return: A pointer to the changed string.
  */
-
 char *cap_string(char *str)
 {
 	int index = 0;
