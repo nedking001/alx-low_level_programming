480e7e44e24293363788418fe42d2310f7135e6e
Chinedum <chinedume77@gmail.com>
2024-07-08 21:41:33 -0700

    task9

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * set_string - function to process
+  *
+  * @s: Parameter 1
+  *
+  * @to: Parameter 2
+  *
+  * Return: Always 0.
+  */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+
+}
+
