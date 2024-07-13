0efe6f86de35f6fee06166accd87d97351b56f3d
Chinedum <chinedume77@gmail.com>
2024-07-12 19:20:45 -0700

    task1static

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Name of the static library
+LIB_NAME="liball.a"
+
+# Remove any existing static library with the same name
+rm -f $LIB_NAME
+
+# Compile all .c files in the current directory into object files
+gcc -c *.c
+
+# Create the static library from the object files
+ar rcs $LIB_NAME *.o
+
+# Clean up the object files
+rm -f *.o
+
+echo "Static library $LIB_NAME created successfully."
+
