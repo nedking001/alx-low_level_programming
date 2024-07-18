3d248c1acd34af93f1a8b3cda29ec00d988daf91
Chinedum <chinedume77@gmail.com>
2024-07-17 21:51:13 -0700

    newtask101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,45 +1,76 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * argstostr - concatenates all the arguments of the program
- * @ac: int input, number of arguments
- * @av: double pointer array, array of arguments
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
  *
- * Return: pointer to the new string, or NULL if it fails
+ * Return: number of words
  */
-char *argstostr(int ac, char **av)
+int count_word(char *s)
 {
-	int i, n, r = 0, l = 0;
-	char *str;
+	int flag, c, w;
 
-	if (ac == 0 || av == NULL)
-	return (NULL);
+	flag = 0;
+	w = 0;
 
-	for (i = 0; i < ac; i++)
+	for (c = 0; s[c] != '\0'; c++)
 	{
-
-		for (n = 0; av[i][n]; n++)
-		l++;
-	}
-	l += ac;
-
-	str = malloc(sizeof(char) * (l + 1));
-	if (str == NULL)
-	return (NULL);
-
-	for (i = 0; i < ac; i++)
-	{
-
-		for (n = 0; av[i][n]; n++)
+		if (s[c] == ' ')
+		flag = 0;
+		else if (flag == 0)
 		{
-			str[r] = av[i][n];
-			r++;
+			flag = 1;
+			w++;
 		}
-		str[r] = '\n';
-		r++;
 	}
 
-	str[r] = '\0';
-	return (str);
+	return (w);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success) or NULL (Error)
+ */
+char **strtow(char *str)
+{
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	while (*(str + len))
+	len++;
+	words = count_word(str);
+	if (words == 0)
+	return (NULL);
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+	return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+				return (NULL);
+				while (start < end)
+				*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+		start = i;
+	}
+	matrix[k] = NULL;
+
+	return (matrix);
 }
